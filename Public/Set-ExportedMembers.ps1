893757b57c12f3a85e2af9108e102d660e1f4a73
Jan Hoek <github@uncommonsense.nl>
2018-04-03 21:06:12 +0200

    Implemented Set-ExportedMembers

---
+++ Public/Set-ExportedMembers.ps1
@@ -0,0 +1,27 @@
+function Set-ExportedMembers
+{
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]]$Path
+    )
+
+    process
+    {
+        foreach ($Item in $Path)
+        {
+            Import-Module $Item -Force
+
+            $ModuleName = Split-Path -Path $Item -LeafBase
+            $FunctionNames = Get-Command -Module $ModuleName -CommandType Function | Select-Object -ExpandProperty Name
+            $AliasNames = Get-Command -Module $ModuleName -CommandType Alias | Select-Object -ExpandProperty Name
+            $CmdletNames = Get-Command -Module $ModuleName -CommandType Cmdlet | Select-Object -ExpandProperty Name
+
+            Update-ModuleManifest `
+                -Path $Item `
+                -FunctionsToExport $FunctionNames `
+                -AliasesToExport $AliasNames `
+                -CmdletsToExport $CmdletNames
+        }
+    }
+}
