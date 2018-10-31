40ecfb2f4631e9919a2ed0f8b6da0a544bbb7451
Jan Hoek <github@uncommonsense.nl>
2018-10-31 15:36:09 +0100

    First attempt at converting Psake build file to VS Code task file

---
+++ Public/ConvertTo-VSCodeTask.ps1
@@ -0,0 +1,69 @@
+<#
+.EXAMPLE
+Get-PsakeScriptTasks -buildFile ./build.ps1 | ConvertTo-VSCodeTask | ConvertTo-Json | Set-Content ./.vscode/tasks.json
+#>
+function ConvertTo-VSCodeTask
+{
+    param
+    (
+        # Sadly, Get-PsakeScriptTasks returns vanilla PSCustomObjects,
+        # so no possiblity for meaningful type checking here
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [psobject[]]$PsakeTask
+    )
+
+    begin
+    {
+        $CachedPsakeTasks = New-Object System.Collections.ArrayList 
+    }
+
+    process 
+    {
+        $CachedPsakeTasks.AddRange($PsakeTask)
+    }
+
+    end
+    {
+        @{
+            version = '2.0.0'
+            windows = @{
+                options = @{
+                    shell = @{
+                        executable = 'powershell.exe'
+                        args       = '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command'
+                    }
+                }
+            }
+            linux   = @{
+                options = @{
+                    shell = @{
+                        executable = '/usr/bin/pwsh'
+                        args       = '-NoProfile', '-Command'
+                    }
+                }
+            }
+            osx     = @{
+                options = @{
+                    shell = @{
+                        executable = '/usr/local/bin/pwsh'
+                        args       = '-NoProfile', '-Command'
+                    }
+                }
+            }
+            tasks   = $CachedPsakeTasks.ForEach{
+                @{
+                    label          = $_.Name
+                    type           = 'shell'
+                    problemMatcher = @(
+                        '$msCompile'
+                    )
+                    command        = "Invoke-Psake -taskList $($_.Name)"
+                }
+            }
+        }
+    }
+}
+
+Get-PSakeScriptTasks -buildFile '/Users/jhoek/GitHub/UncommonSense.ALPS/build.ps1' | 
+    ConvertTo-VSCodeTask | 
+    ConvertTo-Json -Depth 10
