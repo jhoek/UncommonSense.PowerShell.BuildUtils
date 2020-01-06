f392036d0c35ce8f32fe231575b1ab4d768913ed
Jan Hoek <github@uncommonsense.nl>
2020-01-06 08:45:35 +0100

    Added New-BinaryModuleManifest.ps1

---
+++ Public/New-BinaryModuleManifest.ps1
@@ -0,0 +1,42 @@
+function New-BinaryModuleManifest 
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory)]
+        [string]$ModulePath,
+
+        [Parameter(Mandatory)]
+        [string]$Guid,
+
+        [string]$Author,
+        [string]$CompanyName,
+        [string]$Copyright
+    )
+
+    $ModuleName = (Get-Item -Path $ModulePath).Name
+    $OutputFolderPath = Split-Path -Path $Path -Parent
+    $RootModuleFileName = "$($ModuleName).dll"
+    $RootModulePath = Join-Path -Path $OutputFolderPath -ChildPath $RootModuleFileName
+    $ModuleVersion = (Get-Item -Path $RootModulePath).VersionInfo.ProductVersion
+    $TypesPath = Join-Path -Path $ModulePath -ChildPath 'types.ps1xml'
+    $TypesToProcess = if (Test-Path -Path $TypesPath) { "$ModuleName.types.ps1xml" }
+    $FormatsPath = Join-Path -Path $ModulePath -ChildPath 'format.ps1xml'
+    $FormatsToProcess = if (Test-Path -Path $FormatsPath) { "$ModuleName.format.ps1xml" }
+    $SourcePaths = Get-ChildItem -Path $ModulePath -Filter *.cs -Recurse
+    $CmdletNames = $SourcePaths | Select-String -Pattern '^\s+\[Cmdlet\(Verbs[^.]+\.(\w+),\s+\"(.*)\"' | ForEach-Object { $_.Matches[0] } | ForEach-Object { "$($_.groups[1].Value)-$($_.groups[2].Value)" }
+
+    New-ModuleManifest `
+        -Path $Path `
+        -RootModule $RootModuleFileName `
+        -ModuleVersion $ModuleVersion `
+        -Guid $Guid `
+        -Author $Author `
+        -CompanyName $CompanyName `
+        -Copyright $Copyright `
+        -TypesToProcess $TypesToProcess `
+        -FormatsToProcess $FormatsToProcess `
+        -CmdletsToExport $CmdletNames
+}
--- UncommonSense.PowerShell.BuildUtils.psd1
+++ UncommonSense.PowerShell.BuildUtils.psd1
@@ -69,7 +69,7 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = 'Get-ModuleVersion', 'Set-ExportedModuleMembers', 'Step-ModuleVersion', 'Set-AssemblyInfoVersion', 'ConvertTo-VSCodeTask', 'Update-VSCodeTask'
+    FunctionsToExport = 'Get-ModuleVersion', 'Set-ExportedModuleMembers', 'Step-ModuleVersion', 'Set-AssemblyInfoVersion', 'ConvertTo-VSCodeTask', 'Update-VSCodeTask', 'New-BinaryModuleManifest'
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport   = @()
